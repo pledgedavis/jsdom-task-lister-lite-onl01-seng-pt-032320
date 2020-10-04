578a91dbda6e2b9ff29e589e3d62ae48f8cd4301
pledgedavis <pledgedavis14@gmail.com>
2020-10-04 01:22:49 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // const firstButtonPage = document.querySelector("buttonclicked")
+  // const newTaskForm = document.getElementById("create-task-form");
+
+
+  // firstButtonPage.addEventListener('click' , () => {
+  //   alert("I was clicked")
+  // })
+
+
+
+// your code here
+const createTaskForm = document.getElementById("create-task-form");
+createTaskForm.addEventListener("submit", createNewTask);
+console.log("hello")
 });
+
+const createNewTask = event => {
+   //stop form from trying to submit
+event.preventDefault();
+
+const newTaskDescription = document.getElementById("new-task-description");
+const newTask = document.createElement("li");
+newTask.innerText = newTaskDescription.value;
+
+appendNewTask(newTask);
+event.target.reset();
+};
+
+const appendNewTask = task => {
+document.getElementById("tasks").appendChild(task);
+
+  
+  };
